0ba6137d08412eef7af07203d02e2e454c8f291c
Salman <salmanajani0426@gmail.com>
2023-01-28 12:50:18 +0530

    added basic components

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import TopicsProviders from "./context/TopicsContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <TopicsProviders>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </TopicsProviders>
   );
 }
