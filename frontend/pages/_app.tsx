1d71bbbd3c63a36f650eb986065812a3d88ded47
Harsha Umesh <harshau129@gmail.com>
2023-01-28 14:16:57 +0530

    backend

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
